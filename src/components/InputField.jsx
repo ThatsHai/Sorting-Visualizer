e9e33128ce74c2c51b0c5a01a41c77c503631725
Hai Nguyen <nguyendonghaiwork94@gmail.com>
2024-07-31 22:30:37 +0700

    Auto generating number

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -6,10 +6,24 @@ const InputField = () => {
   const [error, setError] = useState("");
   const isFocused = useRef({ isFocused: false });
 
+  const generateNumber = () => {
+    setValue("");
+    const randomQuantity = Math.floor(Math.random() * 7 + 5);
+    let newValue = "";
+    for (let i = 0; i < randomQuantity; i++) {
+      const random = Math.floor(Math.random() * 1000);
+      if (i > 0) {
+        newValue += `, ${random.toString()}`;
+      } else {
+        newValue += random.toString();
+      }
+    }
+    setValue(newValue);
+  };
+
   const handleChange = (e) => {
     const inputValue = e.target.value;
     const regex = /^[0-9,]+$/;
-    // console.log(typeof inputValue)
 
     if (regex.test(inputValue) || inputValue == "") {
       if (inputValue.endsWith(",") && inputValue.length == 1) {
@@ -28,6 +42,7 @@ const InputField = () => {
       setError(true);
     }
   };
+
   const handleFocus = () => {
     isFocused.current.isFocused = true;
   };
@@ -37,23 +52,31 @@ const InputField = () => {
   };
 
   return (
-    <div className="input-container relative bg-blue-100 m-4">
-      <label className="sm:text-sm lg:text-lg absolute top-[-50%] left-[2%] px-1 text-gray-500 pointer-events-none z-50">
-        Input number:
-      </label>
+    <div className="menu-container flex">
+      <button
+        className="min-w-10 border-solid border-2 text-gray-500 border-gray-500 h-fit w-fit p-3"
+        onClick={generateNumber}
+      >
+        Generate Random Numbers
+      </button>
+      <div className="input-container relative m-4">
+        <label className="sm:text-sm lg:text-lg absolute top-[-50%] left-[2%] px-1 text-gray-500 pointer-events-none z-50">
+          Input number:
+        </label>
 
-      <input
-        placeholder="23,45,13,..."
-        value={value}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        type="text"
-        className={`${
-          isFocused.current.isFocused && error && "focus:border-red-700 "
-        } border-2 border-solid rounded-md border-gray-500 min-w-[30rem] mx-3 p-2 focus:border-gray-700 focus:outline-none focus:ring-0`}
-        ref={isFocused}
-      />
+        <input
+          placeholder="23,45,13,..."
+          value={value}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          type="text"
+          className={`${
+            isFocused.current.isFocused && error && "focus:border-red-700 "
+          } border-2 border-solid rounded-md border-gray-500 min-w-[30rem] mx-3 p-2 focus:border-gray-700 focus:outline-none focus:ring-0`}
+          ref={isFocused}
+        />
+      </div>
     </div>
   );
 };
